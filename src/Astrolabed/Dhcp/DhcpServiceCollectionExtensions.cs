c9cdd0d642d1042901fa0d7c3601152c1268c0a6
John Crocker <182365840+guidcruncher@users.noreply.github.com>
2026-08-12 03:57:33 +0100

    Scaffolding

--- src/Astrolabed/Dhcp/DhcpServiceCollectionExtensions.cs
+++ src/Astrolabed/Dhcp/DhcpServiceCollectionExtensions.cs
@@ -50,7 +50,7 @@ public static class DhcpServiceCollectionExtensions
                 logger);
         });
 
-	services.AddSingleton<IDhcpRuntimeLoader, DhcpRuntimeLoader>();
+        services.AddSingleton<IDhcpRuntimeLoader, DhcpRuntimeLoader>();
         services.AddHostedService<DhcpHostedService>();
 
         return services;
