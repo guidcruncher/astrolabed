961fc070238a73d8a3e8e704f83fcdff6bf53b48
John Crocker <182365840+guidcruncher@users.noreply.github.com>
2026-08-13 06:09:07 +0100

    Scaffolding

--- src/Astrolabed/Metrics/MetricsServicesCollection.cs
+++ src/Astrolabed/Metrics/MetricsServicesCollection.cs
@@ -1,3 +1,4 @@
+using Astrolabed.Data.Repositories;
 using Astrolabed.Events;
 
 using Microsoft.Extensions.Configuration;
@@ -10,6 +11,10 @@ public static class MetricsServiceCollectionExtensions
     public static IServiceCollection AddMetricServices(
         this IServiceCollection services, IConfiguration config)
     {
+
+        services.AddSingleton<IDnsResponseEventRepository, SqliteDnsResponseEventRepository>();
+
+        services.AddSingleton<MetricsRepository>();
         services.AddSingleton<MetricsRegistry>();
         services.AddSingleton<IEventConsumer, MetricsEventConsumer>();
 
