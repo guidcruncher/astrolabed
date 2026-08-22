b0a0a30e00eacb7be70808259d91c0d3e882bfa2
John Crocker <182365840+guidcruncher@users.noreply.github.com>
2026-08-22 02:43:40 +0100

    fix

--- src/Astrolabed.Dns/Extensions/ServiceCollectionExtensions.cs
+++ src/Astrolabed.Dns/Extensions/ServiceCollectionExtensions.cs
@@ -85,7 +85,7 @@ public static class ServiceCollectionExtensions
         services.AddSingleton<IUpstreamClientFactory, UpstreamClientFactory>();
 
         // Client name resolver
-        services.AddTransient<NetworkScannerService, NetworkScannerService>();
+        services.AddTransient<INetworkScannerService, NetworkScannerService>();
 
         services.AddSingleton<IClientNameResolver, ClientNameResolver>();
 
