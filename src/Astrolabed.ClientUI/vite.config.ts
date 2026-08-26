04c7ea7b001814c24731f9abd736e59f2beff3aa
John Crocker <182365840+guidcruncher@users.noreply.github.com>
2026-08-26 15:40:31 +0100

    Fixs

--- src/Astrolabed.ClientUI/vite.config.ts
+++ src/Astrolabed.ClientUI/vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import Components from 'unplugin-vue-components/vite'
+import path from 'path'
 
 export default defineConfig({
   plugins: [
