66bfe04b68a90a3f3a014af9dabb6ea48f4a3acd
John Crocker <182365840+guidcruncher@users.noreply.github.com>
2026-08-31 12:56:26 +0100

    Added date filters

--- src/Astrolabed.Data/Repositories/IStatsRepository.cs
+++ src/Astrolabed.Data/Repositories/IStatsRepository.cs
@@ -12,13 +12,17 @@ public interface IStatsRepository
     /// detailing blocked and allowed counts per hour.
     /// </summary>
     /// <param name="cancellationToken">Cancellation token to cancel the operation.</param>
+    /// <param name="startEpoch">Start Epoch to filter by</param>
+    /// <param name="endEpoch">End Epoch to filter by</param>
     /// <returns>A collection of exactly 24 hourly event metrics.</returns>
-    Task<IReadOnlyCollection<DnsHourlyEventSummary>> GetHourlyEventSummariesAsync(CancellationToken cancellationToken = default);
+    Task<IReadOnlyCollection<DnsHourlyEventSummary>> GetHourlyEventSummariesAsync(long startEpoch, long endEpoch, CancellationToken cancellationToken = default);
 
     /// <summary>
     /// Gets a breakdown of DNS queries grouped by their question type.
     /// </summary>
     /// <param name="cancellationToken">Cancellation token to cancel the operation.</param>
+    /// <param name="startEpoch">Start Epoch to filter by</param>
+    /// <param name="endEpoch">End Epoch to filter by</param>
     /// <returns>A collection of DNS question type summaries detailing counts per record type.</returns>
-    Task<IEnumerable<DnsQuestionTypeSummary>> GetQuestionTypeSummary(CancellationToken cancellationToken = default);
+    Task<IEnumerable<DnsQuestionTypeSummary>> GetQuestionTypeSummary(long startEpoch, long endEpoch, CancellationToken cancellationToken = default);
 }
