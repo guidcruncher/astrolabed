2253104d6deff00aebf47c4f8f9961e24ed25e2c
John Crocker <182365840+guidcruncher@users.noreply.github.com>
2026-09-01 19:25:48 +0100

    Fixy

--- src/Astrolabed.ClientUI/src/views/DashboardView.vue
+++ src/Astrolabed.ClientUI/src/views/DashboardView.vue
@@ -26,6 +26,9 @@
               @date-selected="handleDateSelected"
             />
           </div>
+	  <div v-if="blockRate">
+	    Block Rate: {{ blockRate.blockRatePercentage }}%
+          </div>
         </Panel>
       </div>
 
@@ -80,7 +83,6 @@
         </Panel>
       </div>
     </div>
-
     <div class="h-[380px]">
       <StackedBarChart
         :show-legend="false"
@@ -147,6 +149,7 @@ import { useApi } from '../composables/useApi'
 import { useDateUtils } from '../composables/useDateUtils'
 
 import type {
+  BlockRateResponse,
   DnsQuestionTypeSummary,
   AstrolabedStatusResponse,
   DnsHourlyEventSummary,
@@ -156,13 +159,13 @@ import { Globe, Clock } from '@lucide/vue'
 
 const { formatUtcToLocalBrowserTime } = useDateUtils()
 const { getDnsTypeColorConfig } = useDnsTypeColor()
-const { getCurrentTime, getDnsQuestionTypeSummary, getDnsHourlyEventSummary } = useApi()
+const { getBlockRate, getCurrentTime, getDnsQuestionTypeSummary, getDnsHourlyEventSummary } = useApi()
 const hourDnsData = ref<DnsHourlyEventSummary[] | null>(null)
 const questionTypeData = ref<DnsQuestionTypeSummary[] | null>(null)
 const currentTime = ref<Date | null>(null)
 const startDate = ref<number>(0)
 const endDate = ref<number>(0)
-
+const blockRate = ref<BlockRateResponse>()
 const loading = ref<boolean>(false)
 const error = ref<string | null>(null)
 
@@ -223,6 +226,7 @@ const fetchData = async (): Promise<void> => {
     const startOfDate = startDate.value * 1000
     const now = await getCurrentTime()
     currentTime.value = new Date(now)
+    blockRate.value = await getBlockRate()
     hourDnsData.value = await getDnsHourlyEventSummary(startOfDate, endOfDate)
     questionTypeData.value = await getDnsQuestionTypeSummary(startOfDate, endOfDate)
   } catch (err) {
